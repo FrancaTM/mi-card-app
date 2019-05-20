1387076dfea9b050dffdb32b532c57c850317a07
Túlio <francatm@outlook.com>
2019-05-20 18:14:43 -0300

    Just some refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -17,89 +17,81 @@ class MyApp extends StatelessWidget {
     emailAddress: emailAddressMock,
   );
 
+  Widget _buildText(
+      String text, String fontFamily, double fontSize, Color color,
+      [double letterSpacing = 1.0]) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: fontFamily,
+        fontSize: fontSize,
+        color: color,
+        fontWeight: FontWeight.bold,
+        letterSpacing: letterSpacing,
+      ),
+    );
+  }
+
+  Widget _buildCard(IconData icon, String text) {
+    return Card(
+      margin: EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 25.0,
+      ),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: primaryColor,
+        ),
+        title: Text(
+          text,
+          style: TextStyle(
+            color: secondaryColorShade900,
+            fontFamily: fontFamilySecondary,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBox() {
+    return SizedBox(
+      height: 20.0,
+      width: 150.0,
+      child: Divider(
+        height: 1.0,
+        color: secondaryColorShade100,
+      ),
+    );
+  }
+
+  Widget _buildLayout() {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage(person.circleAvatarImg),
+          ),
+          _buildText(person.name, fontFamilyPrimary, 40.0, Colors.white),
+          _buildText(person.jobTitle, fontFamilySecondary, 20.0,
+              secondaryColorShade100, 2.5),
+          _buildBox(),
+          _buildCard(Icons.phone, person.phoneNumber),
+          _buildCard(Icons.email, person.emailAddress),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: primaryColor,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage(person.circleAvatarImg),
-              ),
-              Text(
-                person.name,
-                style: TextStyle(
-                  fontFamily: fontFamilyPrimary,
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                  fontFamily: fontFamilySecondary,
-                  fontSize: 20.0,
-                  color: secondaryColorShade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  height: 1.0,
-                  color: secondaryColorShade100,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: primaryColor,
-                  ),
-                  title: Text(
-                    person.phoneNumber,
-                    style: TextStyle(
-                      color: secondaryColorShade900,
-                      fontFamily: fontFamilySecondary,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: primaryColor,
-                  ),
-                  title: Text(
-                    person.emailAddress,
-                    style: TextStyle(
-                      color: secondaryColorShade900,
-                      fontFamily: fontFamilySecondary,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: _buildLayout(),
       ),
     );
   }
