c3b8bf4a33fa784c363cf7e885315719633bd53d
Túlio <francatm@outlook.com>
2019-05-20 09:44:29 -0300

    Add widgets and change properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String circleAvatarImgUrl =
+      'https://yt3.ggpht.com/a/AGF-l7_p35s8NY-EOwhcv9cgmj1RB2s4jkQyR7YoKQ=s900-mo-c-c0xffffffff-rj-k-no';
+  final String circleAvatarImg = 'images/tulio.jpg';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(circleAvatarImg),
+              ),
+              Text(
+                'Túlio M. França',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
